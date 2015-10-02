860c628cbf9c01a8541a857f29284b8d3353ba47
somename <someemail>
2015-10-02 12:31:18 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -67,6 +67,8 @@ public class TestSudokuVerifier extends TestCase {
 		String invalidSudoku = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 		
 		int res = verifier.verify(invalidSudoku);
+		
+		this.assertNotSame(res, SudokuVerifier.VALID);
 	}
 
 }
