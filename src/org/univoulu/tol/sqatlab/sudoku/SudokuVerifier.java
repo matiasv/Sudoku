6c3e99033cd93dfd677866cc318a47040ccdf59f
matiasv <matias.vatanen@gmail.com>
2015-10-02 13:50:01 +0300

    Implemented checking of rows and cols and tests for them
    yep.

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -57,7 +57,23 @@ public class SudokuVerifier {
 		}
 	}
 	
-	private static boolean checkSubGrid(String[][] sudokuGrid) {
+	private static boolean checkSubGrids(String[][] sudokuGrid) {
+		
+		return false;
+	}
+	/*
+	 *    0  1  2  3  4  5  6  7  8
+	 * 0| 0  0  0  1  1  1  2  2  2  
+	 * 1| 0  0  0  1  1  1  2  2  2
+	 * 2| 0  0  0  1  1  1  2  2  2
+	 * 3| 3  3  3  4  4  4  5  5  5
+	 * 4| 3  3  3  4  4  4  5  5  5
+	 * 5| 3  3  3  4  4  4  5  5  5
+	 * 6| 6  6  6  7  7  7  8  8  8
+	 * 7| 6  6  6  7  7  7  8  8  8
+	 * 8| 6  6  6  7  7  7  8  8  8
+	 */
+	private static boolean checkSubGrid(String[][] sudokuGrid,int indx) {
 		
 		return false;
 	}
