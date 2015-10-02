002771011c024ab33b09fbc349c83464ad3268b2
somename <someemail>
2015-10-02 11:46:26 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -7,6 +7,10 @@ import org.junit.Test;
 public class TestSudokuVerifier {
 	private SudokuVerifier verifier;
 	
+	@Override
+	public void setUp() throws Exception {
+		verifier = new SudokuVerifier();
+	}
 	
 	@Test
 	public void test() {
