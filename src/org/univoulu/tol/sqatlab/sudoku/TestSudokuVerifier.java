bc2c5f3e4cdb016b8324ea19f6eb0da3eb790347
somename <someemail>
2015-10-02 12:30:26 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -66,6 +66,7 @@ public class TestSudokuVerifier extends TestCase {
 	public void testSudokuInvalidRows() {
 		String invalidSudoku = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 		
+		int res = verifier.verify(invalidSudoku);
 	}
 
 }
